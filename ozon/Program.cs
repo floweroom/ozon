e2f449b2b477985167cbd31e9fcb509c2638b463
karpenkoalena493 <karpenkoalena493@gmail.com>
2024-02-11 17:15:16 +1000

    hh

--- ozon/Program.cs
+++ ozon/Program.cs
@@ -1,25 +1,92 @@
-﻿
-//var number = int.Parse(Console.ReadLine());
+﻿using System.ComponentModel.Design;
 
-//for (var i = 0; i < number; i++)
+using var input = new StreamReader(Console.OpenStandardInput());
+using var output = new StreamWriter(Console.OpenStandardOutput());
+var number = int.Parse(input.ReadLine());
 
-//{
-//    //Console.WriteLine("Введите дату");
-//    string massive;
-//    massive = Console.ReadLine();
+for (var i = 0; i < number; i++)
 
-//    string separators = " ";
+{
 
-//    string[] year = massive.Split(separators, StringSplitOptions.RemoveEmptyEntries);
-
-//    int y = Convert.ToInt32(year[year.Length - 1]);
-//    bool t = true;
-//    t = y % 4 == 0 || y % 400 == 0;
-//    Console.WriteLine(t ? "yes" : "no");
-//}
+    var massive = input.ReadLine();
 
 
-//return;
+    string separators = " ";
+
+    string[] year = massive.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+    
+    
+    if (Convert.ToInt32(year[year.Length - 2]) == 1 && Convert.ToInt32(year[year.Length - 3]) <= 31)
+    {
+        Console.WriteLine("yes");
+    }
+
+    else if (Convert.ToInt32(year[year.Length - 2]) == 3 && Convert.ToInt32(year[year.Length - 3]) <= 31)
+    {
+        Console.WriteLine("yes");
+    }
+    else if (Convert.ToInt32(year[year.Length - 2]) == 4 && Convert.ToInt32(year[year.Length - 3]) <= 30)
+    {
+        Console.WriteLine("yes");
+    }
+    else if (Convert.ToInt32(year[year.Length - 2]) == 5 && Convert.ToInt32(year[year.Length - 3]) <= 31)
+    {
+        Console.WriteLine("yes");
+    }
+    else if (Convert.ToInt32(year[year.Length - 2]) == 6 && Convert.ToInt32(year[year.Length - 3]) <= 30)
+    {
+        Console.WriteLine("yes");
+    }
+
+    else if (Convert.ToInt32(year[year.Length - 2]) == 7 && Convert.ToInt32(year[year.Length - 3]) <= 31)
+    {
+
+        Console.WriteLine("yes");
+    }
+    else if (Convert.ToInt32(year[year.Length - 2]) == 8 && Convert.ToInt32(year[year.Length - 3]) <= 31)
+    {
+
+        Console.WriteLine("yes");
+    }
+    else if (Convert.ToInt32(year[year.Length - 2]) == 9 && Convert.ToInt32(year[year.Length - 3]) <= 30)
+    {
+        Console.WriteLine("yes");
+    }
+    else if (Convert.ToInt32(year[year.Length - 2]) == 10 && Convert.ToInt32(year[year.Length - 3]) <= 31)
+    {
+        Console.WriteLine("yes");
+    }
+    else if (Convert.ToInt32(year[year.Length - 2]) == 11 && Convert.ToInt32(year[year.Length - 3]) <= 30)
+    {
+        Console.WriteLine("yes");
+    }
+    else if (Convert.ToInt32(year[year.Length - 2]) == 12 && Convert.ToInt32(year[year.Length - 3]) <= 31)
+
+    {
+        Console.WriteLine("yes");
+    }
+
+    else if (Convert.ToInt32(year[year.Length - 2]) == 2 && Convert.ToInt32(year[year.Length - 3]) <= 29 && Convert.ToInt32(year[year.Length - 1]) % 4 == 0 || Convert.ToInt32(year[year.Length - 1]) % 400 == 0)
+    {
+        Console.WriteLine("yes");
+    }
+    else if (Convert.ToInt32(year[year.Length - 2]) == 2 && Convert.ToInt32(year[year.Length - 3]) <= 28 && Convert.ToInt32(year[year.Length - 1]) % 4 != 0 || Convert.ToInt32(year[year.Length - 1]) % 400 != 0)
+    {
+        Console.WriteLine("yes");
+    }
+
+
+
+    else
+    {
+        Console.WriteLine("no");
+    }
+
+
+}
+
+    
+return;
 
 
 //string number = Console.ReadLine();
@@ -86,24 +153,47 @@ output.Write(a + b);
 //    output.WriteLine(m);
 
 
-using static System.Runtime.InteropServices.JavaScript.JSType;
+//using static System.Runtime.InteropServices.JavaScript.JSType;
 
-using var input = new StreamReader(Console.OpenStandardInput());
-using var output = new StreamWriter(Console.OpenStandardOutput());
+//using var input = new StreamReader(Console.OpenStandardInput());
+//using var output = new StreamWriter(Console.OpenStandardOutput());
 
-int day = int.Parse(Console.ReadLine()); 
-for (var i = 0; i < day; i++)
-{
-    int max = 30;
-    int min = 15;
-    int worker = int.Parse(Console.ReadLine());
-    for (var j = 0; j < worker; j++)
-    {
-        
-    }
+//int day = int.Parse(Console.ReadLine());
+//for (var i = 0; i < day; i++)
+//{
+//    int max = 30;
+//    int min = 15;
+//    int worker = int.Parse(Console.ReadLine());
+//    for (var j = 0; j < worker; j++)
+//    {
+//        string temp = Console.ReadLine();
+//        string separators = " ";
+
+//        string[] temperatur = temp.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+
+//        max = Math.Min(temperatur[1], max);
 
 
-}
+//        //    if (temperatur[0].EndsWith(">=") && int.Parse(temperatur[1]) > 15)
+//        //    {
+//        //        Console.WriteLine(temperatur[1]);
+//        //    }
 
-//4 1 >= 30 6 >= 18 <= 23 >= 20 <= 27 <= 21 >= 28
+//        //          if (temperatur[0].EndsWith(">=") && int.Parse(temperatur[1]) > )
+
+
+//        //         if (y > max)
+//        //            {
+//        //              -1;
+//        //            }
+//        //                  if (temp < min)
+//        //{
+
+//        //}
+//    }
+
+
+//}
+
+//// 6 >= 18 <= 23 >= 20 <= 27 <= 21 >= 28
 
